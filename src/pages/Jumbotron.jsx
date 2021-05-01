b64e3a58962516d02070f02aa1c88f60e88bdffb
Adnan Zahir <adnanzaidanz@gmail.com>
2021-05-01 17:51:14 +0700

    jumbotron

--- src/pages/Jumbotron.jsx
+++ src/pages/Jumbotron.jsx
@@ -1,15 +1,19 @@
+import jumbotronWebp from '../images/jumbotron/jumbotron.webp';
 import jumbotronPng from '../images/jumbotron/jumbotron.png';
-import jumbotronSmallPng from '../images/jumbotron/jumbotron-small.png';
+import jumbotronSmallWebp from '../images/jumbotron/jumbotron-small.webp';
+import jumbotronSmallPng from '../images/jumbotron/jumbotron-small.webp';
 
 const Jumbotron = () => {
   return (
     <section className="jumbotron full-height">
       <picture>
+        <source media="(min-width: 900px)" srcSet={jumbotronWebp} />
         <source media="(min-width: 900px)" srcSet={jumbotronPng} />
+        <source media="(max-width: 899px)" srcSet={jumbotronSmallWebp} />
         <img src={jumbotronSmallPng} alt="Calon mempelai sedang bersama" />
       </picture>
       <h1 className="sub-title">The Wedding of</h1>
-      <h1 className="title">Adnan & Aisha</h1>
+      <h1 className="title">Aku & Kamu</h1>
     </section>
   );
 };
