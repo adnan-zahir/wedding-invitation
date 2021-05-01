32a4e92cd796c415993edfe6a91f2bc8c5305a6b
Adnan Zahir <adnanzaidanz@gmail.com>
2021-05-01 15:25:27 +0700

    + added video container

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -6,9 +6,11 @@ const Video = () => {
       <h2 className="section-title" data-aos="fade-down">
         When We Decide to be Together
       </h2>
-      <video data-aos="fade" controls height="250" width="300">
-        <source src={videoMp4} type="video/mp4" />
-      </video>
+      <div className="video-container">
+        <video data-aos="fade" controls height="250" width="300">
+          <source src={videoMp4} type="video/mp4" />
+        </video>
+      </div>
       <p data-aos="fade-up">Everything starts from here</p>
     </section>
   );
